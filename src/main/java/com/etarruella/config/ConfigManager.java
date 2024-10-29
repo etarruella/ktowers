bdeaf645c943545ea885d068630edd490ac1741d
Erik Tarruella <eriktarruella@gmail.com>
2024-10-29 21:36:31 +0100

    !feat(config): BREAKING CHANGE ConfigManager before refactor

--- src/main/java/com/etarruella/config/ConfigManager.java
+++ src/main/java/com/etarruella/config/ConfigManager.java
@@ -1,33 +1,34 @@
 package com.etarruella.config;
 
+import com.etarruella.utils.FileManager;
+import org.bukkit.Bukkit;
+import org.bukkit.WorldCreator;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 // TODO: Add connection to FileManager (unzip & files management)
 public class ConfigManager {
 
-    private final static String MAPS_DIR = "maps";
-    private final static String MAPS_CFG_DIR = "maps/cfg";
-    private final static String DEFAULT_MAP_NAME = "ktowers";
-
     private static ConfigManager configManager;
+
+    private final Plugin plugin;
 
     private Map<String, File> maps;
     private Map<String, FileConfiguration> mapsCfg;
     private FileConfiguration currentMapConfig;
-    private Plugin plugin;
 
     public ConfigManager(Plugin plugin) {
         configManager = this;
 
         this.plugin = plugin;
 
-        maps = new HashMap<>();
-        mapsCfg = new HashMap<>();
+        FileManager.saveDefaultResources();
+
+        mapLoader();
     }
 
     public FileConfiguration getMainConfig() {
@@ -38,11 +39,20 @@ public class ConfigManager {
         return mapsCfg.get(name);
     }
 
-    public void setCurrentMapConfig(String name) {
+    public void setCurrentMap(String name) {
         currentMapConfig = mapsCfg.get(name);
     }
 
-    public FileConfiguration getCurrentMapConfig() {
+    public void loadMap(String name) {
+        FileManager.prepareMap(maps.get(name));
+        Bukkit.getWorlds().add(new WorldCreator(name).createWorld());
+    }
+
+    /**
+     * Gets the config of the current loaded map
+     * @return The FileConfiguration of the map
+     */
+    public FileConfiguration getCurrentMapCfg() {
         return currentMapConfig;
     }
 
@@ -52,6 +62,20 @@ public class ConfigManager {
 
     public static ConfigManager getConfigManager() {
         return configManager;
+    }
+
+    /**
+     * 1st -> Get the (.zip) maps in the MainConfig.ACTIVE_MAPS List that has a (.yml) config
+     * 2nd -> Load the first map on the ACTIVE_MAPS List
+     * 3rd -> Get the configurations
+     */
+    private void mapLoader() {
+        List<String> activeMaps = MainConfig.ACTIVE_MAPS.getValue();
+
+        maps = FileManager.getMaps();
+        setCurrentMap(activeMaps.getFirst());
+
+        mapsCfg = FileManager.getMapConfigs();
     }
 
 }
