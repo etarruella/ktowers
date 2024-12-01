651aae07891edc2d6514b3c31706e4b8892f1487
Erik Tarruella <eriktarruella@gmail.com>
2024-12-01 19:03:39 +0100

    feat: cleanup PlayerManager, adding isPlayerLogged and preparePlayer methods

--- src/main/java/com/etarruella/core/PlayerManager.java
+++ src/main/java/com/etarruella/core/PlayerManager.java
@@ -1,6 +1,8 @@
 package com.etarruella.core;
 
+import com.etarruella.config.MapMetadata;
 import com.etarruella.core.entities.KPlayer;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -16,13 +18,22 @@ public class PlayerManager {
         return players.computeIfAbsent(playerId, id -> new KPlayer(player));
     }
 
-    public static void deletePlayer(KPlayer kPlayer) {
-        players.remove(kPlayer.getUuid());
+    public static boolean isPlayerLogged(Player player) {
+        return players.containsKey(player.getUniqueId());
+    }
+
+    public static void deletePlayer(Player player) {
+        players.remove(player.getUniqueId());
     }
 
     private static void addKPlayer(Player player) {
         KPlayer kPlayer = new KPlayer(player);
         players.put(kPlayer.getUuid(), kPlayer);
+    }
+
+    public static void preparePlayer(KPlayer kPlayer) {
+        Location lobbyLocation = MapMetadata.LOBBY_GENERAL.getValue();
+        kPlayer.getPlayer().teleport(lobbyLocation);
     }
 
 }
