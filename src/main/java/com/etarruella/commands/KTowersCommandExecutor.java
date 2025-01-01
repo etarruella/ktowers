929f7b9bf4b380b1094475b63d37a38e43c4903d
Erik Tarruella <eriktarruella@gmail.com>
2025-01-01 23:44:24 +0100

    fix: return correct subcommands list onTabComplete

--- src/main/java/com/etarruella/commands/KTowersCommandExecutor.java
+++ src/main/java/com/etarruella/commands/KTowersCommandExecutor.java
@@ -3,7 +3,9 @@ package com.etarruella.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class KTowersCommandExecutor implements KTowersCommand {
 
@@ -31,6 +33,15 @@ public class KTowersCommandExecutor implements KTowersCommand {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        switch (args.length) {
+            case 1:
+                return Arrays.stream(SubCommands.values())
+                        .map(SubCommands::toString)
+                        .collect(Collectors.toList());
+            case 2:
+                // List of maps
+                break;
+        }
         return List.of();
     }
 
